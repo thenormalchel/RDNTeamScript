738d048428642a7bace967a4d5398ced33af0c14
RDNTeam <157903540+thenormalchel@users.noreply.github.com>
2024-08-29 15:02:37 +0300

    rdnscript.lua

---
+++ rdnscript.lua
@@ -0,0 +1,5 @@
+-- script by RDNTeam https://discord.gg/VF7gxQMA --
+-- SPEEDHACK --
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+
+print("Script by RDNTeam , discord - https://discord.gg/VF7gxQMA")
