2aa9dced6cde1341803111f4d2a7349f5629b8e9
RDNTeam <157903540+thenormalchel@users.noreply.github.com>
2024-08-29 21:14:14 +0300

    Update rdnscript.lua

--- rdnscript.lua
+++ rdnscript.lua
@@ -3,31 +3,3 @@
 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
 
 print("Script by RDNTeam , discord - https://discord.gg/VF7gxQMA")
-local RunService = game:GetService('RunService')
-local LocalPlayer = game:GetService('Players').LocalPlayer
-local Camera = workspace.CurrentCamera
-local Obscuring = {}
-
-RunService.RenderStepped:connect(function()
-    for _, v in pairs(Obscuring) do 
-        v.LocalTransparencyModifier = 0
-        for _, v2 in pairs(v:GetChildren()) do
-            if v2:IsA('Texture') then
-                v2.Transparency /= 1000
-            end
-        end
-    end
-
-    if not LocalPlayer.Character then return end
-    Obscuring = Camera:GetPartsObscuringTarget({LocalPlayer.Character.HumanoidRootPart.CFrame.p}, LocalPlayer.Character:GetChildren())
-
-    for _, v in pairs(Obscuring) do 
-        v.LocalTransparencyModifier = 1
-        for _, v2 in pairs(v:GetChildren()) do
-            if v2:IsA('Texture') then
-                v2.Transparency *= 1000
-            end
-        end
-    end    
-end)
-
